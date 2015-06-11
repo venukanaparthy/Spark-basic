f0179c59c3abf9778017689524f9a866101e65f2
Venu Kanaparthy <vkanaparthy@esri.com>
2015-06-10 17:55:32 -0700

    parse nyc taxi trips

---
+++ spark_trips.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext
+
+baseDir = os.path.join('data')
+inputPath = os.path.join('trips.csv')
+fileName = os.path.join(baseDir, inputPath)
+tripsRDD = sc.textFile(fileName,8).map(parse_trips).reduceByKey(lambda x, y: x+y )
+
+#first 10 trips
+trips10RDD = tripsRDD.take(10)
+print trips10RDD
+
+#aggregate trips by year
+import 
+tripsByYearRDD = tripsRDD.map(lambda ((c,r,d),v): (d[:4], v)).reduceByKey(lambda x, y: x+y )
+print tripsByYearRDD
